1564eef5ad9e238f51ebdedde9c023a90e59ee6c
Luis Caraballo <luisred61@gmail.com>
2020-03-25 17:44:27 -0500

    Route and view added for products

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const productsRouter = require('./routes/productsRouter')
 
-app.get('/', function (req, res, next) {
-    res.send({ hello: "hello" });
-});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");    //Including pug template engine
+
+app.use('/products', productsRouter)
 
 const server = app.listen(8000, function () {
     console.log(`Listening http://localhost:${server.address().port}`)
